67a1b799ccdf028abb20ac6a7f78642950858a0f
sakshicpatel <sakshicpatel@gmail.com>
2025-09-19 15:04:13 +0530

    commit
    i want to commit and push

---
+++ mouse.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(20);
+}
+
+function draw() {
+if (mouseIsPressed) {
+fill(50, 100, 200, 100);//semi-transparent blue
+noStroke();
+ellipse(mouseX, mouseY, 20, 20)//draw a circle at mouse
+ }
+}
